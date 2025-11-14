4410d24126b4c53696da3133805a4d960c6453d4
bob-dev <boristraore225@gmail.com>
2025-11-14 16:29:36 +0000

    fastapi + react deployment test setup

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+origins = ["http://localhost:5173"]
+
+app.add_middleware(CORSMiddleware, 
+    allow_origins= origins,
+    allow_headers= ["*"],
+    allow_methods = ["*"],
+    allow_credentials = True
+)
+
+
+@app.get("/")
+async def home():
+    return {"data" : "Ceci est mon 1er site déployé"}
